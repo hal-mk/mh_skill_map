5c192708b8a7d0928d4d418a3e0f48eeaa0aede8
halcy <halcyon.muku74@outlook.jp>
2021-06-05 19:39:07 +0900

    master

---
+++ mh_skill_map/mh_skill_map/App/MainPanel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace mh_skill_map.App
+{
+    public partial class MainPanel : Form
+    {
+        public MainPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void weaponBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
